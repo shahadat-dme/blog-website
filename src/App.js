7ac1ff330620ee19e1171ee85e77a5937673eec7
Shahadat <shahadat2cp17@gmail.com>
2022-01-01 12:28:37 +0600

    app

--- src/App.js
+++ src/App.js
@@ -22,12 +22,17 @@ function App() {
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/createpost">Create Post</Link>
-        { !isAuth ? <Link to="/login">LogIn</Link> : <button onClick={signUserOut}> Log Out</button>}
+        { !isAuth ? 
+        (<Link to="/login">LogIn</Link> ):( 
+          <>
+            <Link to="/createpost">Create Post</Link>
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
       </nav>
       <Routes>
        <Route path="/" element={<Home/>} />
-        <Route path="/createpost" element={<CreatePost/>} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>} />
         <Route path="/login" element={<Login setIsAuth={setIsAuth}/>} /> 
       </Routes>
     </Router> 
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import { db, auth } from "../firebase.config"
+import { useNavigate } from "react-router-dom"
 
-function CreatePost() {
+function CreatePost({ isAuth }) {
+    const [title, setTitle] = useState("");
+    const [postText, setPostText] = useState("");
+
+    const postsCollectionRef = collection(db,"posts")
+    let navigate = useNavigate();
+
+    const createPost = async () => {
+        await addDoc(postsCollectionRef,{
+            title, postText, author: {name: auth.currentUser.displayName, id: auth.currentUser.uid},
+        });
+        navigate("/");
+    };
+
+    useEffect(() => {
+        if (!isAuth){
+            navigate("/login")
+        }
+    }, [])
     return (
         <>
         <div className="createPostPage">
@@ -8,16 +29,17 @@ function CreatePost() {
                 <h1>Create A Post</h1>
                 <div className="inputGp">
                     <label> Title: </label>
-                    <input placeholder='Title...'/>
+                    <input placeholder='Title...' 
+                    onChange={(e) => {setTitle(e.target.value)}}/>
                 </div>
                 <div className="inputGp">
                     <label> Post: </label>
-                    <textarea placeholder='Post...'/>
+                    <textarea placeholder='Post...'
+                    onChange={(e) => {setPostText(e.target.value)}}/>
                 </div>
-                <button>Submit</button>
+                <button onClick={createPost}>Submit</button>
             </div>
         </div>
-
         </>
 
 
