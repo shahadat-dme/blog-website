eb94d853fbd49dc2b62879efd1cfc99c1cdf0cf9
Shahadat <shahadat2cp17@gmail.com>
2022-01-01 15:38:14 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import Home from './pages/Home';
-import CreatePost from './pages/CreatePost';
-import Login from './pages/Login';
-import { useState } from 'react';
-import { signOut } from "firebase/auth"
-import { auth } from './firebase.config';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
-  const [ isAuth, setIsAuth] = useState(false)
-
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
 
   const signUserOut = () => {
-    signOut(auth).then(() =>{
-      localStorage.clear()
-      setIsAuth(false)
-      window.location.pathname = "/login"
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
     });
   };
+
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>
-        { !isAuth ? 
-        (<Link to="/login">LogIn</Link> ):( 
+        <Link to="/"> Home </Link>
+
+        {!isAuth ? (
+          <Link to="/login"> Login </Link>
+        ) : (
           <>
-            <Link to="/createpost">Create Post</Link>
+            <Link to="/createpost"> Create Post </Link>
             <button onClick={signUserOut}> Log Out</button>
           </>
         )}
       </nav>
       <Routes>
-       <Route path="/" element={<Home isAuth={isAuth}/>} />
-        <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>} />
-        <Route path="/login" element={<Login setIsAuth={setIsAuth}/>} /> 
+        <Route path="/" element={<Home isAuth={isAuth} />} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
-    </Router> 
+    </Router>
   );
 }
 
