aa508df2a6494e3979e3d058df131b7642ddafbb
Shahadat <shahadat2cp17@gmail.com>
2022-01-01 15:12:56 +0600

    again

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
       localStorage.clear()
       setIsAuth(false)
       window.location.pathname = "/login"
-    })
-  }
+    });
+  };
   return (
     <Router>
       <nav>
@@ -31,7 +31,7 @@ function App() {
         )}
       </nav>
       <Routes>
-       <Route path="/" element={<Home/>} />
+       <Route path="/" element={<Home isAuth={isAuth}/>} />
         <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>} />
         <Route path="/login" element={<Login setIsAuth={setIsAuth}/>} /> 
       </Routes>
