7bcd7548cc26d0c48c2ea4cfe7632aaba6401651
Shahadat <shahadat2cp17@gmail.com>
2022-01-01 12:43:15 +0600

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+import {getDocs, collection} from "firebase/firestore"
+import { db } from "../firebase.config"
 
 function Home() {
+    const [ postLists, setPostLists] = useState([]);
+    const postsCollectionRef = collection(db,"posts")
+
+    useEffect (() => {
+        const getPosts = async () => {
+            const data = await getDocs(postsCollectionRef)
+            console.log(data)
+        }
+        getPosts();
+    })
     return (
         <div className='homePage'>
             Home
